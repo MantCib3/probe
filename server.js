a4ff165b27144b81fd80bbcaa481aef264d5ca91
MantCib3 <mattaiton567123@gmail.com>
2026-09-10 07:02:39 -0700

    csp: allow fonts.googleapis.com (style-src) and fonts.gstatic.com (font-src) so Bebas Neue/Inter/Outfit load instead of silently falling back to system fonts

--- server.js
+++ server.js
@@ -2184,7 +2184,8 @@ function proxyImage(rawUrl, res) {
 const CSP = [
   "default-src 'self'",
   "script-src 'self' https://challenges.cloudflare.com",
-  "style-src 'self' 'unsafe-inline'",
+  "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
+  "font-src 'self' https://fonts.gstatic.com",
   "img-src 'self' data: https:",
   "connect-src 'self' https://challenges.cloudflare.com",
   "frame-src https://challenges.cloudflare.com",
